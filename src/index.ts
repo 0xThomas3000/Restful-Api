76515539be9295b58afc7e8965d2a5c08b04da46
tuanit168 <tuanit168@gmail.com>
2022-06-06 14:23:32 +0700

    1. Setup MongoDB and Configure to get connected to it

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,41 @@
-const n: string = "Tuan";
-console.log(n);
+import dotenv from "dotenv";
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+import morgan from "morgan";
+
+dotenv.config();
+
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(morgan("dev"));
+app.use(express.json());
+app.use(express.urlencoded());
+
+// Database
+const URI = process.env.MONGODB_URL;
+mongoose.connect(
+  URI!,
+  {
+    autoIndex: false,
+  },
+  // Callback function
+  (err) => {
+    if (err) throw err;
+    console.log("MongoDB is now connected successfully !");
+  }
+);
+
+// Routes
+app.get("/", (req, res) => {
+  res.json({ msg: "Hello Tuan" });
+});
+
+// Start Server listening
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => {
+  console.log(`Express is listening on port ${port}`);
+});
